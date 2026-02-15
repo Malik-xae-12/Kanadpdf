2d704e430aff1bb2e642f9d22991a7162b74d4c8
Malik-xae-12 <az786muzaffar@gmail.com>
2026-02-15 14:59:59 +0530

    final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
       "/api": {
         target: "https://kanadpdf-1.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
